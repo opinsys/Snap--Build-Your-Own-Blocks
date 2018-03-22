3f5aa996ff48eb30c35e6355751d5da4401de035
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-03-22 15:32:42 +0200

    Change the width of the palette.
    Old commits:
        7d2afb4141f59dffd02751a50dbe7defd453544d
        d218c77b2bd19a7e34dcba1dbc58924444491f20
    
    The old commit no longer works at all. It messes up the UI.

--- gui.js
+++ gui.js
@@ -252,7 +252,7 @@ IDE_Morph.prototype.init = function (isAutoFill) {
     this.hasChangedMedia = false;
 
     this.isAnimating = true;
-    this.paletteWidth = 200; // initially same as logo width
+    this.paletteWidth = 400; // initially same as logo width
     this.stageRatio = 1; // for IDE animations, e.g. when zooming
 
 	this.wasSingleStepping = false; // for toggling to and from app mode
